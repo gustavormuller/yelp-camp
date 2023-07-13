4365b9b65dcdafa383a96f0db1b23d0894b772f0
unknown <gusrmuller@gmail.com>
2023-07-13 03:52:54 -0300

    add images

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,           
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit leo et arcu interdum elementum. Nullam feugiat augue ut sem accumsan, at sodales nunc vestibulum. Nam tincidunt tempor purus, nec pharetra sapien volutpat et. Mauris et quam eget purus feugiat auctor.',
+            price
         })
         await camp.save();
     }
